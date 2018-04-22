f48e43731ed2d38d2ec47d9d434509a709e6e445
Erin Pentecost <erin@pentecost.email>
2018-04-22 01:47:29 -0700

    jitter fix

--- SCENES/Bullets/Translate.gd
+++ SCENES/Bullets/Translate.gd
@@ -8,11 +8,13 @@ signal TranslationFinishedSignal(sender)
 
 export var speed = 4.0
 #how tight the pather handles corners. value between 0 and 1
-export var accl = 0.3
-export var rotAccl = 0.08
+export var accl = 0.52
+export var rotAccl = 0.002
 
 export(NodePath) var spatialToMove = NodePath("../")
 var  __spatialNode = null
+
+var __leftoverSpeed = 0.0
 
 var velocity = Vector3(0.0, 0.0, 0.0)
 var rotVelocity = Vector3(0.0, 0.0, 0.0)
@@ -31,7 +33,8 @@ func ClearWaypoints():
 func _ready():
 	self.__spatialNode = self.get_node(spatialToMove)
 
-func _process(delta):
+
+func __move(delta):
 	#print("" + self.get_path() + " has " + str(waypoints.size()) + " points to go to" )
 	if (waypoints.size() != 0):
 		# Do we need to translate from spatial to vector3?
@@ -43,14 +46,22 @@ func _process(delta):
 			#print(str(self.get_path()) + " at ("+str(__spatialNode.global_transform.origin) + ") ->  ("+str(targetCoord)+")" )
 		
 		#move towards target destination
-		var howFarWeWillMove = velocity * delta
-		var direction = howFarWeWillMove.normalized()
-		var maxSpeed = min(howFarWeWillMove.length(), targetCoord.distance_to(__spatialNode.global_transform.origin))
-		var actualVelocity = direction * maxSpeed
+		var projectedVelocity = velocity * delta
+		var direction = projectedVelocity.normalized()
+		var projectedDistance = projectedVelocity.length()
+		var maxDistance = targetCoord.distance_to(__spatialNode.global_transform.origin)
+		var maxSpeed = min(projectedDistance, maxDistance)
+		if (maxDistance < projectedDistance):
+			velocity = direction * maxSpeed
+			__leftoverSpeed = projectedDistance - maxDistance
+		else:
+			velocity = projectedVelocity
+			__leftoverSpeed = 0.0
 		
-		__spatialNode.global_translate(howFarWeWillMove)
+		__spatialNode.global_translate(projectedVelocity)
+
 		# look at new direction
-		__spatialNode.look_at(rotVelocity + __spatialNode.global_transform.origin, Vector3(0, 1, 0))
+		__spatialNode.look_at((rotVelocity * delta) + __spatialNode.global_transform.origin, Vector3(0, 1, 0))
 		
 		#remove waypoint once we get close
 		if (__closeEnough(__spatialNode.global_transform.origin, targetCoord)):
@@ -61,9 +72,14 @@ func _process(delta):
 			#start going to next waypoint if there is one
 			var newDirection = Vector3(targetCoord - __spatialNode.global_transform.origin).normalized()
 			velocity = ((newDirection * accl) + (velocity * (1 - accl))).normalized() * speed
-			rotVelocity = ((newDirection * rotAccl) + (velocity * (1 - rotAccl))).normalized() * speed
+			rotVelocity = ((newDirection * rotAccl) + (velocity * (1 - rotAccl))).normalized()
 	else:
 		emit_signal("TranslationFinishedSignal", self)
+
+func _process(delta):
+	__move(delta)
+	while (__leftoverSpeed > 0.0):
+		__move(delta)
 
 func __closeEnough(vectorA, vectorB):
 	var distance = vectorA.distance_to(vectorB)
