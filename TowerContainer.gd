61cf6ae429062f556631f2459e98879027ed2124
Erin Pentecost <erin@pentecost.email>
2018-04-22 18:06:38 -0700

    tower now owns bullets

--- TowerContainer.gd
+++ TowerContainer.gd
@@ -11,19 +11,18 @@ func shootAt(baddie):
 	if (!canShoot(baddie)):
 		return
 	var bullet = __createBullet()
-	print (_description() + "shooting at " + str(baddie) + " with " + str(bullet))
+	print (_description() + " shooting at " + str(baddie) + " with " + str(bullet))
 	bullet.shootAt(baddie)
 
 func __createBullet():
 	var nBullet = (load(bulletPath)).instance()
 	#todo: orient to this spatial's facing
-	nBullet.transform.origin = self.transform.origin
-	
+	nBullet.global_transform.origin = self.global_transform.origin
+	self.add_child(nBullet)
 	return nBullet
 
 func _ready():
 	print("new "+_description())
-	pass
 
 func _description():
-	return str(self.towerType) +" tower at " + str(self.transform.origin)
+	return str(self.towerType) +" tower at " + str(self.global_transform.origin)
