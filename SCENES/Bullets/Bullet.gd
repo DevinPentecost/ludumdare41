9ecc90c1d3d59ed07bf2770289ac143dacf077a7
Erin Pentecost <erin@pentecost.email>
2018-04-22 20:07:57 -0700

    we are basically done now

--- SCENES/Bullets/Bullet.gd
+++ SCENES/Bullets/Bullet.gd
@@ -3,12 +3,14 @@ extends Spatial
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+var __intendedTarget = null
 
 func shootAt(baddie):
 	print("bullet at "+str(self.transform.origin) + " will shoot " + str(baddie) )
 	var wayFinder = self.get_node("WaypointFollower")
 	wayFinder.AppendWaypoint(baddie)
 	wayFinder.connect("TranslationFinishedSignal", self, "__missingTargetHandler")
+	__intendedTarget = baddie
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -16,8 +18,10 @@ func _ready():
 	print("bullet at "+str(self.transform.origin))
 	pass
 
-func __missingTargetHandler():
+func __missingTargetHandler(a):
 	print("bullet has nowhere to go")
+	if (__intendedTarget != null):
+		__intendedTarget.takeDamage(1)
 	self.queue_free()
 
 func _process(delta):
